d0642e2c34274c0f259a6b5cb62b830e126defd2
Rick <1358735+rickb777@users.noreply.github.com>
2020-12-29 16:05:04 +0000

    added some missing type assertions

--- immutable/assertions.go
+++ immutable/assertions.go
@@ -1,7 +1,19 @@
 package immutable
 
+var _ AnyCollection = &AnyList{}
+var _ AnyCollection = &AnySet{}
+
 var _ IntCollection = &IntList{}
 var _ IntCollection = &IntSet{}
 
+var _ Int64Collection = &Int64List{}
+var _ Int64Collection = &Int64Set{}
+
 var _ StringCollection = &StringList{}
 var _ StringCollection = &StringSet{}
+
+var _ UintCollection = &UintList{}
+var _ UintCollection = &UintSet{}
+
+var _ Uint64Collection = &Uint64List{}
+var _ Uint64Collection = &Uint64Set{}
